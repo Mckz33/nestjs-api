a5d7027aad9c1f5a0fb40b17e7e064c857d0e41b
Mckz33 <mackenzie.max.rj@gmail.com>
2024-03-16 13:44:26 -0300

    Documentando com Swagger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { LogInterceptor } from './interceptors/log.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,15 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe())
 
   app.useGlobalInterceptors(new LogInterceptor);
+
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
